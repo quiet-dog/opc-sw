86bb132f2b57b7f319d1a86dc6134290994e5be1
huyanyou <1026855639@qq.com>
2024-12-11 08:10:12 +0800

    init

--- test/stomp.go
+++ test/stomp.go
@@ -1,88 +1,116 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
-	"time"
+	"net/http"
 
 	"github.com/go-stomp/stomp/v3"
 	"github.com/gorilla/websocket"
 )
 
-// WebSocketWrapper 包装 websocket.Conn 以实现 io.ReadWriteCloser
-type WebSocketWrapper struct {
-	conn *websocket.Conn
+// WebSocketReadWriteCloser 是 gorilla/websocket.Conn 的适配器
+type WebSocketReadWriteCloser struct {
+	Conn *websocket.Conn
 }
 
-func (w *WebSocketWrapper) Read(p []byte) (n int, err error) {
-	// 使用 websocket.Conn 的 ReadMessage 来实现 io.Reader
-	_, p, err = w.conn.ReadMessage()
+// Read 实现 io.Reader 接口
+func (w *WebSocketReadWriteCloser) Read(p []byte) (int, error) {
+	_, message, err := w.Conn.ReadMessage()
+	if err != nil {
+		return 0, err
+	}
+	n := copy(p, message)
+	return n, nil
+}
+
+// Write 实现 io.Writer 接口
+func (w *WebSocketReadWriteCloser) Write(p []byte) (int, error) {
+	err := w.Conn.WriteMessage(websocket.TextMessage, p)
 	if err != nil {
 		return 0, err
 	}
 	return len(p), nil
 }
 
-func (w *WebSocketWrapper) Write(p []byte) (n int, err error) {
-	// 使用 websocket.Conn 的 WriteMessage 来实现 io.Writer
-	err = w.conn.WriteMessage(websocket.TextMessage, p)
-	if err != nil {
-		return 0, err
-	}
-	return len(p), nil
+// Close 实现 io.Closer 接口
+func (w *WebSocketReadWriteCloser) Close() error {
+	return w.Conn.Close()
 }
 
-func (w *WebSocketWrapper) Close() error {
-	return w.conn.Close()
+// DeviceDTO represents a device with various properties
+type DeviceDTO struct {
+	DeviceType           string                  `json:"deviceType"`           // 设备类型
+	DeviceID             int64                   `json:"deviceId"`             // 设备ID
+	EnvironmentAlarmInfo EnvironmentAlarmInfoDTO `json:"environmentAlarmInfo"` // 环境档案数据信息
+	EquipmentInfo        EquipmentInfoDTO        `json:"equipmentInfo"`        // 设备信息
+}
+
+// EnvironmentAlarmInfoDTO represents environment alarm information
+type EnvironmentAlarmInfoDTO struct {
+	EnvironmentID    int64   `json:"environmentId"`    // 设备ID
+	Value            float64 `json:"value"`            // 数据
+	Unit             string  `json:"unit"`             // 单位
+	Power            float64 `json:"power"`            // 功耗
+	WaterValue       float64 `json:"waterValue"`       // 用水量
+	ElectricityValue float64 `json:"electricityValue"` // 用电量
+}
+
+// EquipmentInfoDTO represents equipment information
+
+// EquipmentInfoDTO represents equipment information
+type EquipmentInfoDTO struct {
+	EquipmentID int64   `json:"equipmentId"` // 设备ID
+	ThresholdID int64   `json:"thresholdId"` // 阈值传感器ID
+	SensorName  string  `json:"sensorName"`  // 传感器名称
+	Value       float64 `json:"value"`       // 传感器值
 }
 
 func main() {
-	// 连接 WebSocket 服务器
-	wsConn, _, err := websocket.DefaultDialer.Dial("ws://localhost:9020/ws", nil)
+	// WebSocket 连接信息
+	url := "ws://localhost:9020/ws"
+	header := http.Header{}
+
+	// 使用 gorilla/websocket 连接到服务器
+	conn, _, err := websocket.DefaultDialer.Dial(url, header)
 	if err != nil {
-		log.Fatal("Failed to connect to WebSocket server: ", err)
+		log.Fatalf("Failed to connect to WebSocket server: %v", err)
 	}
-	defer wsConn.Close()
-	fmt.Println("=323232")
+	defer conn.Close()
 
-	// 将 WebSocket 连接包装为实现了 io.ReadWriteCloser 接口的类型
-	wrappedConn := &WebSocketWrapper{conn: wsConn}
+	// 包装 WebSocket 连接为 io.ReadWriteCloser
+	rwc := &WebSocketReadWriteCloser{Conn: conn}
 
-	// 使用封装后的连接连接到 STOMP 协议
-	conn, err := stomp.Connect(wrappedConn)
-	fmt.Println("=132123323232")
+	// 使用 STOMP 客户端连接
+	stompConn, err := stomp.Connect(rwc)
 	if err != nil {
-		log.Fatal("Failed to connect with STOMP protocol: ", err)
+		log.Fatalf("Failed to connect to STOMP: %v", err)
 	}
-	defer conn.Disconnect()
-	fmt.Println("=12313")
-	// 订阅消息
-	subscription, err := conn.Subscribe("/topic/info", stomp.AckAuto)
+	defer stompConn.Disconnect()
+
+	log.Println("Connected to STOMP server")
+
+	// 订阅主题
+	sub, err := stompConn.Subscribe("/topic/info", stomp.AckAuto)
 	if err != nil {
-		log.Fatal("Failed to subscribe: ", err)
+		log.Fatalf("Failed to subscribe to topic: %v", err)
 	}
-	defer subscription.Unsubscribe()
+	defer sub.Unsubscribe()
 
-	// 启动一个goroutine处理接收到的消息
-	go func() {
-		for {
-			// 接收消息
-			message, err := subscription.Read()
-			if err != nil {
-				log.Fatal("Failed to read message: ", err)
-			}
-			fmt.Printf("Received message: %s\n", string(message.Body))
-		}
-	}()
+	log.Println("Subscribed to /topic/example")
+	d := DeviceDTO{}
+	d.DeviceID = 1
+	d.DeviceType = "test"
+	d.EquipmentInfo.EquipmentID = 1
+	d.EquipmentInfo.SensorName = "test"
+	d.EquipmentInfo.ThresholdID = 1
+	d.EquipmentInfo.Value = 1
+	d.EnvironmentAlarmInfo.EnvironmentID = 1
+	jsonStr, _ := json.Marshal(d)
+	stompConn.Send("/app/deviceInfo", "application/json", jsonStr)
 
-	fmt.Println("=到爱上对方")
-	// 发送消息到Spring Boot服务器
-	err = conn.Send("/app/deviceInfo", "text/plain", []byte("Hello from Go"))
-	if err != nil {
-		log.Fatal("Failed to send message: ", err)
-	}
-	fmt.Println("Message sent to Spring Boot server")
-
-	// 防止主程序退出
-	time.Sleep(10 * time.Second)
+	// // 接收消息
+	// for msg := range sub.C {
+	// 	log.Printf("Received message: %s\n", string(msg.Body))
+	// }
 }
